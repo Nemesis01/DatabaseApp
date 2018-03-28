533607e947f43151227b341dbebc226868380dab
Stéphane Nton <stephane.nton@gmail.com>
2018-03-28 16:17:21 -0400

    Ajout Connexion

---
+++ SMI1002_Project/Models/DatabaseConnection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SMI1002_Project.Models
+{
+    class DatabaseConnection
+    {
+
+        public DatabaseConnection()
+        {
+            OracleConnection conn = new OracleConnection("test");
+        }
+
+    }
+}
